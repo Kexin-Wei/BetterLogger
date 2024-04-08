7dd4764c320f811271834c8e4af0d4c181f648ad
Kexin-Wei <wkx611@outlook.com>
2024-04-08 08:45:41 +0800

    [mod] better main

--- main.cpp
+++ main.cpp
@@ -34,15 +34,12 @@ int main(int argc, char* argv[])
     logger::init_logger();
 
     logger::debug("Logger -- The logger is initialized, {}, {}", 0, 2.3);
-    logger::error("Hello, World!");
-    logger::critical("User did something!");
     logger::error("Some error message with arg: {}", 1);
-
     logger::warn("Easy padding in numbers like {:08d}", 12);
     logger::critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
     logger::info("Support for floats {:03.2f}", 1.23456);
     logger::info("Positional args are {1} {0}..", "too", "supported");
-    logger::info("{:<30}", "left aligned");
+    logger::info("{:<30}", "left aligned\n\n");
 
     // logger::dev::info("I made a developer log!");
     SPDLOG_INFO("I made a developer log!");
